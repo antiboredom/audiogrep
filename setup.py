7957985dcd3fb00fed2b7cd493d0c8767d665ce3
Sam <splavigne@gmail.com>
2015-06-26 03:21:51 -0400

    add manifest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='audiogrep',
-    version='0.1.0',
+    version='0.1.1',
     author='Sam Lavigne',
     author_email='splavigne@gmail.com',
     packages=['audiogrep'],
