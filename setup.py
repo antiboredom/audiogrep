b2d1668ed3fb4e095b334c4cbe3ec26d5f85548c
Sam <splavigne@gmail.com>
2018-02-08 21:01:57 -0500

    fix python3 issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='audiogrep',
-    version='0.1.3',
+    version='0.1.4',
     author='Sam Lavigne',
     author_email='splavigne@gmail.com',
     packages=['audiogrep'],
